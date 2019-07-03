12273b2441d531d2ba9e1475e19b7af90cde365d
bofeiw <bofeiw@icloud.com>
2019-07-03 18:19:26 +1000

    sign name and copy board to s file

--- prog.s
+++ prog.s
@@ -1,11 +1,35 @@
 # COMP1521 19t2 ... Game of Life on a NxN grid
 #
-# Written by <<YOU>>, June 2019
+# Written by <<Bofei Wang>>, June 2019
 
 ## Requires (from `boardX.s'):
 # - N (word): board dimensions
 # - board (byte[][]): initial board state
 # - newBoard (byte[][]): next board state
+
+# boardX.s
+########################################################################
+# board1.s ... Game of Life on a 10x10 grid
+
+	.data
+
+N:	.word 10  # gives board dimensions
+
+board:
+	.byte 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	.byte 1, 1, 0, 0, 0, 0, 0, 0, 0, 0
+	.byte 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+	.byte 0, 0, 1, 0, 1, 0, 0, 0, 0, 0
+	.byte 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+	.byte 0, 0, 0, 0, 1, 1, 1, 0, 0, 0
+	.byte 0, 0, 0, 1, 0, 0, 1, 0, 0, 0
+	.byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+	.byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+	.byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+
+newBoard: .space 100
+
+########################################################################
 
 ## Provides:
 	.globl	main
