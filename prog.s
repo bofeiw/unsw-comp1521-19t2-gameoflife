f5e6379b8936aa0ac190e193eb7bd5adc7ae0fcd
bofeiw <bofeiw@icloud.com>
2019-07-03 18:09:09 +1000

    add init files

---
+++ prog.s
@@ -0,0 +1,41 @@
+# COMP1521 19t2 ... Game of Life on a NxN grid
+#
+# Written by <<YOU>>, June 2019
+
+## Requires (from `boardX.s'):
+# - N (word): board dimensions
+# - board (byte[][]): initial board state
+# - newBoard (byte[][]): next board state
+
+## Provides:
+	.globl	main
+	.globl	decideCell
+	.globl	neighbours
+	.globl	copyBackAndShow
+
+
+########################################################################
+# .TEXT <main>
+	.text
+main:
+
+# Frame:	...
+# Uses:		...
+# Clobbers:	...
+
+# Locals:	...
+
+# Structure:
+#	main
+#	-> [prologue]
+#	-> ...
+#	-> [epilogue]
+
+# Code:
+
+	# Your main program code goes here.  Good luck!
+
+main__post:
+	jr	$ra
+
+	# Put your other functions here
